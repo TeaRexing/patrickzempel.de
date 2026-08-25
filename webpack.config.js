b3e944693a900ecbac75396132f39ee4438cf988
Patrick Zempel <info@patrickzempel.de>
2026-08-25 18:09:47 +0200

    added styleloader for css

--- webpack.config.js
+++ webpack.config.js
@@ -11,16 +11,22 @@ module.exports = {
     symlinks: true
   },
   module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
       },
-    }]
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
